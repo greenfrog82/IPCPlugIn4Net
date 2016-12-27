1062c7b0382f6fa2121a9b09d9d9b28f8cd5026a
DESKTOP-BL8E05C\greenfrog <greenfrog82@naver.com>
2016-12-27 21:41:31 +0900

    Nuget Package에서 System.Diagnostics.Process 패키지를 다운로드 받아서 프로세스를 포크하는 테스트 코드 작성

--- src/AddPlugIn/PlugInImpl.cs
+++ src/AddPlugIn/PlugInImpl.cs
@@ -5,27 +5,24 @@ namespace AddPlugIn
 {
     public class PlugInImpl : IPlugIn
     {
-        
         public PlugInImpl()
         {
-            // TODO .NET Core에서 .NET Framework에서 사용하는 Process.Start라는 메소드를 사용할 수 없는가?
-            //System.Diagnostics.
-            Process.Start
+            
         }
 
         public void PreRun(object param)
         {
-            throw new NotImplementedException();
+            System.Diagnostics.Process.Start("cmd");
         }
 
         public void PostRun()
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public void Run()
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
     }
 }
